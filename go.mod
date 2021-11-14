79b0f1b5521398d498595b3c8ab5595bc735cc1a
JustLinton <530065614@qq.com>
2021-11-14 21:36:03 +0800

    add login/register

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module db_design
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jinzhu/gorm v1.9.16
--- src/main.go
+++ src/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 	"log"
 	"net/http"
 )
 
 // UserInfo 用户信息
-type UserInfo struct {
+type User struct {
 	ID string
 	Name string
-	Gender string
-	Hobby string
+	Phone string
+	Email string
+	PassSHA string
 }
 
 func main() {
@@ -26,11 +30,14 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	db.AutoMigrate(&UserInfo{})
+	db.AutoMigrate(&User{})
 	operateTest(db)
 
 	//初始化API请求处理
 	router := initAPIs(&err,db)
+
+	//设置跨域
+	router.Use(cors.Default())
 
 	// 开始端口监听（进入阻塞）。默认端口是8080,也可以指定端口 r.Run(":80")
 	router.Run(":8080")
@@ -43,18 +50,89 @@ func initAPIs(err *error,db *gorm.DB) *gin.Engine {
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK,"Hello！欢迎来到GO世界！")
 	})
-	router.POST("form", func(c *gin.Context) {
 
-		pname := c.PostForm("pname")
-		pgender := c.PostForm("pgender")
-		phobby := c.PostForm("phobby")
+	router.POST("/login", func(c *gin.Context) {
+
+		passwd := c.DefaultPostForm("passwd","nil")
+		phone := c.DefaultPostForm("phone","nil")
+
+		if passwd=="nil"||phone=="nil" {
+			c.String(http.StatusNotAcceptable, fmt.Sprintln("network"))
+		}
+
+		//sha256 check
+		//passwdBYTE:=[]byte(passwd)
+		//passwdSHA:=GetSHA256HashCode(passwdBYTE)
+		//fmt.Println(passwdSHA)
+
+		var uu = new(User)
+		db.Find(&uu, "pass_sha=?", passwd)
+		//fmt.Printf("pass_sha:%s", uu.Phone)
+		if uu.PassSHA==passwd {
+			//passwd is correct
+			userUid := uuid.Must(uuid.NewV4(),*err)
+			strUsrUid := fmt.Sprintf("%s",userUid)
+			//login success
+			c.Status(http.StatusOK)
+			c.SetCookie("user_token", strUsrUid, 1000, "/", "localhost", false, true)
+
+		}else {
+			//not correct passwd!
+			c.String(http.StatusNotAcceptable, fmt.Sprintln("passwd"))
+		}
+	})
+
+	router.POST("/register", func(c *gin.Context) {
+
+		email := c.DefaultPostForm("email","nil")
+		passwd := c.DefaultPostForm("passwd","nil")
+		name := c.DefaultPostForm("name","nil")
+		phone := c.DefaultPostForm("phone","nil")
+
+		if email=="nil"||passwd=="nil"||phone=="nil"||name=="nil" {
+			c.String(http.StatusNotAcceptable, fmt.Sprintln("network"))
+		}
+
+		//sha256 check
+		//passwdBYTE:=[]byte(passwd)
+		//passwdSHA:=GetSHA256HashCode(passwdBYTE)
+		//fmt.Println(passwdSHA)
+
 		userUid := uuid.Must(uuid.NewV4(),*err)
 
-		fmt.Printf("uuid:%s\n", userUid)
-		strUsrUid := fmt.Sprintf("%s",userUid)
-		entry1 := UserInfo{strUsrUid,pname,pgender,phobby}
-		db.Create(entry1)
+		var uu = new(User)
+		db.Find(&uu, "phone=?", phone)
+		//fmt.Printf("phone:%s", uu.Phone)
+		if uu.Phone!=phone {
+			//phone num haven't used yet
+			fmt.Printf("uuid:%s\n", userUid)
+			strUsrUid := fmt.Sprintf("%s",userUid)
+			newUser := User{strUsrUid, name, phone, email,passwd}
+			db.Create(newUser)
+			//register success
+			c.Status(http.StatusOK)
+			c.SetCookie("user_token", strUsrUid, 1000, "/", "localhost", false, true)
+		}else {
+			//phone num is used!
+			c.String(http.StatusNotAcceptable, fmt.Sprintln("phone"))
+		}
+
 	})
+
+	//router.POST("form", func(c *gin.Context) {
+	//
+	//	pname := c.DefaultPostForm("pname","nil")
+	//	pgender := c.PostForm("pgender")
+	//	phobby := c.PostForm("phobby")
+	//	fmt.Printf("pname:%s\n",pname)
+	//	userUid := uuid.Must(uuid.NewV4(),*err)
+	//	c.String(http.StatusOK, fmt.Sprintln("post 请求  string 格式话"))
+	//
+	//	fmt.Printf("uuid:%s\n", userUid)
+	//	strUsrUid := fmt.Sprintf("%s",userUid)
+	//	entry1 := UserInfo{strUsrUid,pname,pgender,phobby}
+	//	db.Create(entry1)
+	//})
 	return router
 }
 
@@ -66,6 +144,21 @@ func connectDatabase() (*gorm.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+// GetSHA256HashCode SHA256生成哈希值
+func GetSHA256HashCode(message []byte)string{
+	//方法一：
+	//创建一个基于SHA256算法的hash.Hash接口的对象
+	hash := sha256.New()
+	//输入数据
+	hash.Write(message)
+	//计算哈希值
+	bytes := hash.Sum(nil)
+	//将字符串编码为16进制格式,返回字符串
+	hashCode := hex.EncodeToString(bytes)
+	//返回哈希值
+	return hashCode
 }
 
 func operateTest(db *gorm.DB){
